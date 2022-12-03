52ff14f665df1b2c854c019bede953de1d40770e
Gourav Naik <contact@gouravnaik.dev>
2022-12-03 19:08:48 +0530

    Update config.js
    NFT Dashboard

--- src/config.js
+++ src/config.js
@@ -1,22 +1,22 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0xc43FE307B85DA59da23B9d957e9F925B8030Ed40",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
     "block_chain_id": "137",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "NFT5",
 
     // 4. Set your Covalent API Key
-    "api_key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
+    "api_key": "ckey_8e6054f8ee1847c59ddbffd5ed6",
 
     // 5. (Optional) Display the floor price chart
     "timeseries_chart": true,
 
     // 6. (Optional) Set your banner image
-    "banner_picture": "",
+    "banner_picture": "https://ethindia.co/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fhero.fbc70285.png&w=3840&q=75",
   },
 
   // Supported networks - just for your reference
