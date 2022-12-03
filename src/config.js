b00ec284975ef1816d6c297fd0164da9faab00ad
Gourav Naik <contact@gouravnaik.dev>
2022-12-03 19:19:11 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export const CONFIG = {
     "block_chain_id": "137",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "NFT5",
+    "title": "NFT5 V4",
 
     // 4. Set your Covalent API Key
     "api_key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
